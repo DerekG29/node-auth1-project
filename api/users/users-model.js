1956997d8601e05358845bd920a2ad0eef64feea
DerekG29 <derekgrubbs29@gmail.com>
2024-07-26 18:05:58 -0400

    cleaned up users-model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -7,44 +7,27 @@ module.exports = {
   add
 }
 
-/**
-  resolves to an ARRAY with all users, each user having { user_id, username }
- */
-async function find() {
-  const users = await Users('users')
+function find() {
+  return Users('users')
     .select('user_id', 'username');
-  return users;
 }
 
-/**
-  resolves to an ARRAY with all users that match the filter condition
- */
-async function findBy(filter) {
-  const users = await Users('users')
+function findBy(filter) {
+  return Users('users')
     .where(filter)
     .select('user_id', 'username');
-  return users;
 }
 
-/**
-  resolves to the user { user_id, username } with the given user_id
- */
-async function findById(user_id) {
-  const user = await Users('users')
+function findById(user_id) {
+  return Users('users')
     .where({ user_id })
     .first()
     .select('user_id', 'username')
-  return user;
 }
 
-/**
-  resolves to the newly inserted user { user_id, username }
- */
 async function add(user) {
   const [id] = await Users('users')
     .insert(user)
 
   return findById(id)
 }
-
-// Don't forget to add these to the `exports` object so they can be required in other modules
